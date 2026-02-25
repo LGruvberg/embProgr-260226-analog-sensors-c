6f5c3d4c09a45e88d29f4c16eb28ddc8f893d213
Joakim Englund <joakimenglund@protonmail.com>
2026-02-25 22:15:20 +0100

    Initial commit for avr_temp_sensor

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 ## Uppgift
 
-Koppla in en LED matris och en analog joystick till en arduino. Skriv ut en etta på samma sätt som en tärning på LED matrisen.
-Klickar man joysticken åt något håll ska talet ändras och det nya värdet skrivas ut på LED matrisen.
-Rörs joysticken åt höger ska 1 adderas till talet, åt vänster ska talet 1 subtraheras från talet, uppåt ska 2 adderas till talet, och ner ska 2 subtraheras från talet. 
+Koppla in OUT-pinnen från en analog temperatursensor till en analog input pin på en arduino, VCC till 5V, och GND till jord. Skriv ut värdet från den analoga input pinnen. Klicka på temp-sensorn när programmet körs och flytta på slidern för att se värdet skifta.
 
 ## Wokwi
 
-https://wokwi.com/projects/456953521227841537
+https://wokwi.com/projects/456961801070147585
 
 
 ## Branches
 
 Detta repo innehåller flera grenar för de olika situationer vi kommer titta på
-
-**OBS!** I denna branch tittar vi på en lite modifierad version av Stefans kod som printar både till serial monitor och till en LED matris som vi kommer gå in på mer nästa vecka.
 
 
 ## make kommandon
--- src/main.c
+++ src/main.c
@@ -1,144 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <avr/interrupt.h>
-#include <stdbool.h>
 #include "analogRead.h"
 #include "millis.h"
 #include "uart.h"
-#include "max72xx.h"
 
-#define VERT_PIN 0
-#define HORZ_PIN 1
+#define IN_PIN PC0
 
 #define BIT_SET(a, b) (a |= (1U << b))
 #define BIT_CLEAR(a, b) (a &= ~(1U << b))
 #define BIT_FLIP(a, b) (a ^= (1U << b))
 #define BIT_CHECK(a, b) (a & (1U << b))
 
-void clear(){
-	for(int col = 3; col < 6; col++)
-	{
-		max7219b_clr(col, 3);
-		max7219b_clr(col, 4);
-		max7219b_clr(col, 5);
-	}
-}
+int main() {
+	BIT_CLEAR(DDRC, IN_PIN);
 
-void draw(int tal) {
-	if(tal == 1) {
-		max7219b_set(4, 4);
-	}
-	else if(tal == 2) {
-		max7219b_set(3, 3);
-		max7219b_set(5, 5);
-	}
-	else if(tal == 3) {
-		max7219b_set(3, 3);
-		max7219b_set(4, 4);
-		max7219b_set(5, 5);
-	}
-	else if(tal == 4) {
-		max7219b_set(3, 3);
-		max7219b_set(3, 5);
-		max7219b_set(5, 3);
-		max7219b_set(5, 5);
-	}
-	else if(tal == 5) {
-		max7219b_set(3, 3);
-		max7219b_set(3, 5);
-		max7219b_set(4, 4);
-		max7219b_set(5, 3);
-		max7219b_set(5, 5);
-	}
-	else if(tal == 6) {
-		max7219b_set(3, 3);
-		max7219b_set(3, 4);
-		max7219b_set(3, 5);
-		max7219b_set(5, 3);
-		max7219b_set(5, 4);
-		max7219b_set(5, 5);
-	}
-	max7219b_out();
-}
-
-int main()
-{
-	BIT_CLEAR(DDRC, VERT_PIN);
-	BIT_CLEAR(DDRC, HORZ_PIN);
-
- 	millis_init();
-    sei();
-
+	millis_init();
+	sei();
 	init_serial();
-	max7219_init();
 
-	int tal = 1;
-	bool change = true;
-	millis_t millis_since_last_read = 0;
+	uint16_t value = 0;
+	float celsius = 0;
+	int16_t integer = 0;
+	int16_t decimals = 0;
+	millis_t millis_since_last_print = 0;
 	millis_t current_millis = 0;
-	uint16_t oldhorz = 0;
-	uint16_t oldvert = 0;
 
 	while (1) {
 		current_millis = millis();
+		value = analogRead(IN_PIN);
 
-		// Joysticken kommer kontinuerligt ge värden, så vi läser bara var 170:e ms så att vi kan gå ett steg i taget
-		if (current_millis - millis_since_last_read > 170) {
-			millis_since_last_read = current_millis;
-
-			// Läs joystick
-			uint16_t horz = analogRead(HORZ_PIN);
-			uint16_t vert = analogRead(VERT_PIN);
-
-			// Printa horz och vert, men bara om de ändrats
-			if (horz != oldhorz) {
-				printf("Horz: %u\n", horz);
-				oldhorz = horz;
-			}
-			if (vert != oldvert) {
-				printf("Vert: %u\n", vert);
-				oldvert = vert;
-			}
-
-			// Kontrollera om vi läst en signal som innebär modifiering av tal.
-			// LEFT: -1, RIGHT: +1, UP: +2, DOWN: -2
-			if (horz < 300) { // RIGHT
-				change = true;
-
-				if (++tal == 7) {
-					tal = 1;
-				}
-			}
-			else if (horz > 700) { // LEFT
-				change = true;
-
-				if (--tal == 0) {
-					tal = 6;
-				}
-			}
-
-			if (vert > 700) { // UP
-				change = true;
-				tal += 2;
-
-				if (tal > 6) {
-					tal -= 6;
-				}
-			}
-			else if (vert < 300) { // DOWN
-				change = true;
-				tal -= 2;
-
-				if (tal < 1) {
-					tal += 6;
-				}
-			}
-
-			if (change) {
-				change = false;
-				clear();
-				printf("tal: %d\n", tal);
-				draw(tal);
-			}
+		// Printa indatan varje sekund
+		if (current_millis - millis_since_last_print >= 1000) {
+			// Beräkningen är tagen från wokwis beskrivning av komponenten
+			celsius = 1 / (log(1 / (1023. / value - 1)) / 3950. + 1.0 / 298.15) - 273.15;
+			// Speciallösning då vårat väldigt simpla uart-bibliotek inte kan printa flyttal
+			integer = (int)celsius;
+			decimals = abs((int)((celsius - integer) * 100));
+			printf("Current value: %d.%d\n", integer, decimals);
+			millis_since_last_print = current_millis;
 		}
 	}
 	return 0;
